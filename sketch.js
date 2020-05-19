8c5dc601e67e3d1c25fbb92ad68e6f44158eecb7
Zakiya435 <63589526+Zakiya435@users.noreply.github.com>
2020-05-19 18:08:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var draws=[];
+var currentPath=[];
+function setup() {
+  canvas = createCanvas(1000, 600);
+  canvas.mousePressed(start);
+}
+
+function draw()
+{
+  background(230,230,230);
+
+  if(mouseIsPressed){
+    var pos = {
+      x:mouseX,
+      y:mouseY
+    }
+    currentPath.push(pos);
+  }
+  
+  strokeWeight(7);
+  noFill();
+  stroke("black");
+ 
+  for(var i=0; i < draws.length;i++)
+  {
+    var path = draws[i];
+    beginShape();
+    for(var j=0;j<path.length;j++)
+    {
+      vertex(path[j].x,path[j].y);
+    }
+    endShape();
+  }
+
+}
+function start()
+{
+  currentPath=[];
+  draws.push(currentPath);
+}
